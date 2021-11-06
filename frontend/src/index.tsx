a643450c37a80f53fc9d5a032c07b903c50ca192
marcos andrade <marcosjuremao@hotmail.com>
2021-11-06 16:03:19 -0300

    configuracao front end

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import App from './App';
 
 
@@ -9,4 +10,6 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+
 
